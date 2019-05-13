921617e728e75e38bfad20d9546e64cbf25eb087
Phu <phugnguyen@gmail.com>
2019-05-13 10:42:35 -0700

    start dashboard component

---
+++ frontend/src/components/dashboard/dashboard_component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { withRouter } from "react-router-dom";
+// import SearchBar
+// import OutfitShowComponent
+
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  // componentDidMount(){
+  //   this.props.fetchOutfits()
+  // }
+
+  render(){
+      return ();
+  }
+}
+
+export default withRouter(Dashboard);
