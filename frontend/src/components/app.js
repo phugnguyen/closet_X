8a44248d8417b6d0d6e913fd964603ee60229d2f
briceyokoyama <briceyokoyama@gmail.com>
2019-05-13 13:50:18 -0700

    Boilerplate

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 
-import MainPage from './main/main_page';
+import Main from './main/Main';
 
 const App = () => (
     <Switch>
-        <AuthRoute exact path="/" component={MainPage} />
+        <AuthRoute exact path="/" component={Main} />
     </Switch>
 );
 
