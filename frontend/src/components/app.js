b4a0fc21d590111ab8f9b17c1c291705656a7497
Phu <phugnguyen@gmail.com>
2019-05-13 13:59:21 -0700

    merge stuff

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -3,18 +3,12 @@ import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import { Switch } from "react-router-dom";
 
 import MainPage from "./main/main_page";
-<<<<<<< HEAD
 import Dashboard from "./dashboard/dashboard_container";
-=======
->>>>>>> dashboard
 
 const App = () => (
   <Switch>
     <AuthRoute exact path="/" component={MainPage} />
-<<<<<<< HEAD
     <AuthRoute exact path="/dashboard" component={Dashboard} />
-=======
->>>>>>> dashboard
   </Switch>
 );
 
