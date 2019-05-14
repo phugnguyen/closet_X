899120c7facd8c0f8a45ff0dc29215a14db8073c
Phu <phugnguyen@gmail.com>
2019-05-13 17:12:56 -0700

    sort the way the items appear

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -4,11 +4,13 @@ import { Switch } from "react-router-dom";
 
 // import MainPage from "./main/main_page";
 import Dashboard from "./dashboard/dashboard_container";
+import OutfitShow from "./outfit/outfit_show_component";
 
 const App = () => (
   <Switch>
     {/* <AuthRoute exact path="/" component={MainPage} /> */}
     <AuthRoute exact path="/dashboard" component={Dashboard} />
+    <AuthRoute exact path="/outfits/id" component={OutfitShow} />
   </Switch>
 );
 
---
+++ frontend/src/components/outfit/outfit_show_container.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { connect } from "react-redux";
+import OutfitShow from "./outfit_show_component";
+
+const mSTP = state => {
+  return {};
+};
+
+const mDTP = dispatch => {
+  return {};
+};
+
+export default connect(
+  mSTP,
+  mDTP
+)(OutfitShow);
